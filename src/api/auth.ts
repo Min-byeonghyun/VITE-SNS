608739094bd438c0e6a2b5a2ff2c7c194bb00afd
Min-byeonghyun <qudgus5125@naver.com>
2025-11-09 22:17:46 +0900

    비밀번호 재설정 구현

--- src/api/auth.ts
+++ src/api/auth.ts
@@ -39,3 +39,19 @@ export async function signInWithOAuth(provider: Provider) {
 
   return data;
 }
+//비밀번호 재설정 요청
+export async function requestPasswordResetEmail(email: string) {
+  const { data, error } = await supabase.auth.resetPasswordForEmail(email, {
+    redirectTo: `${import.meta.env.VITE_PUBLIC_URL}/reset-password`,
+  });
+  if (error) throw error;
+  return data;
+}
+
+export async function updatePassword(password: string) {
+  const { data, error } = await supabase.auth.updateUser({
+    password,
+  });
+  if (error) throw error;
+  return data;
+}
